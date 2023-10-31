d58ba56d97a1963f5c515569ba7aa9062fd141d0
Ilya <iam.goryn@gmail.com>
2023-10-31 16:03:13 +0300

    files are created and code is written

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,87 @@
-from flask import Flask
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, redirect, url_for, request, flash
+from page_analyzer import db, html
 
-# Это callable WSGI-приложение
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Welcome to Flask!'
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
+
+@app.get('/')
+def index():
+    return render_template('index.html')
+
+
+@app.post('/urls')
+def post_url():
+    url_entered = request.form.get('url')
+    messages = html.validate_url(url_entered)
+
+    if messages:
+        for message in messages:
+            flash(*message)
+        return render_template('index.html', url=url_entered), 422
+
+    connection - db.connect(app)
+    url_name = html.normalize_url(url_entered)
+    url = db.get_url_by_name(connection, url_name)
+
+    if url:
+        flash('Старница уже существует', 'info')
+    else:
+        url = db.add_url(connection, url_name)
+        flash('Старница успешно добавлена', 'success')
+
+    db.close(connection)
+
+    return redirect(
+        url_for(
+            'show_url',
+            url_id=url.id,
+        )
+    )
+
+
+@app.get('/urls')
+def show_urls():
+    connection = db.connect(app)
+    urls = db.get_urls(connecion)
+    db.close(connection)
+
+    return render_template('urls.html', urls=urls)
+
+@app.get('/urls/<int:url_id>')
+def show_url(url_id):
+    connection - db.connect(app)
+    url = db.get_url_by_id(connection, url_id)
+    url_checks = db.get_checks(conniction, url_id)
+    db.close(connection)
+
+    return render_template(
+        'url.htm',
+        url=url,
+        url_checks=url_checks,
+    )
+
+@app.post('/urls/<int:url_id>/checks')
+def check_url(url_id):
+    connection - db.connect(app)
+    url = db.get_url_by_id(connection, url_id)
+    page_data = html.get_page_data(url.name)
+
+    if not page_data or page_data.get('status_code') != 200:
+        flash('Произошла ошибка при проверке', 'danger')
+    else:
+        db.add_check(connection, url_id, page_data)
+        flash('Страница успешно проверена', 'success')
+
+    url_checks = db.get_checks(connection, url_id)
+    db.close(connection)
+
+    return render_template(
+        'url.html',
+        url=url,
+        url_checks=url_checks,
+    )
