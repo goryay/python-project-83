d47bfc36b718940a95ad9c4b9000443e766291c3
Ilya <iam.goryn@gmail.com>
2023-10-31 16:20:32 +0300

    created file database.py

---
+++ database.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS urls CASCADE;
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE urls (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    created_at DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE url_checks ( 
+    id SERIAL PRIMARY KEY,
+    url_id bigont REFERENCES urls (id),
+    status_code INtEGER,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description text,
+    last_checked_at DATE DEFAULT CURRENT_DATE
+);
