0f5a3fecd9df3f333434dea766eed79b55a82767
Ilya <iam.goryn@gmail.com>
2023-11-09 14:43:34 +0300

    refactoring sql file

--- database.sql
+++ database.sql
@@ -9,7 +9,7 @@ CREATE TABLE urls (
 
 CREATE TABLE url_checks ( 
     id SERIAL PRIMARY KEY,
-    url_id bigont REFERENCES urls (id),
+    url_id bigint REFERENCES urls (id),
     status_code INTEGER,
     h1 VARCHAR(255),
     title VARCHAR(255),
