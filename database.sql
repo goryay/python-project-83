2f0583bab45ddf0d73ed8585ca02b89573b74d4f
Ilya <iam.goryn@gmail.com>
2023-11-09 12:58:47 +0300

    sql file editing

--- database.sql
+++ database.sql
@@ -4,15 +4,15 @@ DROP TABLE IF EXISTS url_checks;
 CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
-    created_at DATE DEFAULT CURRENT_DATE
+    created_at DATE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE url_checks ( 
     id SERIAL PRIMARY KEY,
     url_id bigont REFERENCES urls (id),
-    status_code INtEGER,
+    status_code INTEGER,
     h1 VARCHAR(255),
     title VARCHAR(255),
-    description text,
-    last_checked_at DATE DEFAULT CURRENT_DATE
+    description VARCHAR(255),
+    created_at DATE DEFAULT CURRENT_TIMESTAMP
 );
